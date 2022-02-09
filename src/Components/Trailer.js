e76d68a460c73d17563ae0b592e665821a8887c1
Syed Adnan Ali <61422258+IgniteWebDeveloper@users.noreply.github.com>
2022-02-09 17:28:08 +0530

    Revert "New Update with Context API"
    This reverts commit bad919f49682a8ae5d66eaa60616264f4f4e50ec.

--- src/Components/Trailer.js
+++ src/Components/Trailer.js
@@ -1,26 +1,24 @@
-import React, { useContext, useEffect } from 'react';
-import {State} from '../Context/Context'
+import React, { useEffect } from 'react';
 
-const Trailer = () => {
-  const {trailer} = useContext(State)
-  console.log(trailer);
+const Trailer = ({ trailerData }) => {
+  console.log(trailerData);
 
   return <div className='trailerMain'>
-    {trailer ?
+    {trailerData ?
       <div className="imageMain">
-        {trailer.poster_path ? 
-        <img className='backDrop' src={`https://www.themoviedb.org/t/p/w1920_and_h1080_multi_faces${trailer.poster_path}`} alt="" />
+        {trailerData.poster_path ? 
+        <img className='backDrop' src={`https://www.themoviedb.org/t/p/w1920_and_h1080_multi_faces${trailerData.poster_path}`} alt="" />
         : ''}
         <div className="imageBlur"></div>
-        <div className={(!trailer.videos.results[0] ? "noContent": "content")}>
-          {!trailer.videos.results[0] ? 
+        <div className={(!trailerData.videos.results[0] ? "noContent": "content")}>
+          {!trailerData.videos.results[0] ? 
           <h2>OOPS there is no Trailer Available</h2> 
-          :<iframe className='trailerVideo' width="853" height="480" src={`https://www.youtube.com/embed/${trailer.videos.results[0].key}`} title="YouTube video player" frameborder="4" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; allowfullscreen;" ></iframe>}
+          :<iframe className='trailerVideo' width="853" height="480" src={`https://www.youtube.com/embed/${trailerData.videos.results[0].key}`} title="YouTube video player" frameborder="4" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; allowfullscreen;" ></iframe>}
           <div className="Data">
-            <h1>{trailer.title}</h1>
-            <h4>{trailer.overview}</h4>
+            <h1>{trailerData.title}</h1>
+            <h4>{trailerData.overview}</h4>
             <h5>GENRES</h5>
-            {trailer.genres.map(g=><li>{g.name}</li>)}
+            {trailerData.genres.map(g=><li>{g.name}</li>)}
           </div>
         </div>
       </div>
