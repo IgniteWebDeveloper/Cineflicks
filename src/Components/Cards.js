53425e5fd74c6f8fc33c4b76572e079396023798
Syed Adnan Ali <61422258+IgniteWebDeveloper@users.noreply.github.com>
2023-01-20 16:16:39 +0530

    first commit

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -1,16 +1,16 @@
 import React, { Fragment } from 'react';
 import Loader from '../preview.gif';
-import {Grid } from '@mui/material/';
-import {Pagination} from '@mui/material/';
-import {withStyles} from '@mui/styles';
+import { Grid } from '@mui/material/';
+import { Pagination } from '@mui/material/';
+import { withStyles } from '@mui/styles';
 
 
 
 
 
-const Cards = ({ trending, fetchTrending, totalPage, page, fetchTrailer }) => {  
+const Cards = ({ trending, fetchTrending, totalPage, page, fetchTrailer }) => {
 
-const classes = withStyles();
+  const classes = withStyles();
 
   return <Fragment>
     <div className="mainContent">
@@ -22,34 +22,35 @@ const classes = withStyles();
           <h2>Trending Movies</h2>
           <div className="trendingContainer">
             <Grid container spacing={{ xs: 1, md: 1 }} columns={{ xs: 2, sm: 4, md: 25 }}>
-              {trending.map(movie => 
-              <Grid item xs={2} sm={4} md={4} key={movie.id} >
-                <div onClick={()=>fetchTrailer(movie.id)} className="CARD">
-                  <div className="top">
-                    <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
+              {trending.map(movie =>
+                <Grid item xs={2} sm={4} md={4} key={movie.id} >
+                  <div onClick={() => fetchTrailer(movie.id)} className="CARD">
+                    <div className="top">
+                      <img src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="" />
+                    </div>
+                    <div className="bottom">
+                      <h3>{movie.title || movie.name}</h3>
+                      <p>{movie.release_date}</p>
+                    </div>
                   </div>
-                  <div className="bottom">
-                    <h3>{movie.title || movie.name}</h3>
-                    <p>{movie.release_date}</p>
-                  </div>
-                </div>
-              </Grid>)}
+                </Grid>)}
             </Grid>
           </div>
           <div className="pagination">
-            <Pagination 
+            <Pagination
               count={totalPage}
-              className="paginationMain" 
-              style={{backgroundColor: 'rgba(165, 165, 165, 0.886)', 
-              height: "45px", 
-              display:"flex",
-              alignItems:"center",
-             borderRadius: '5px'}} 
-             onChange={fetchTrending}
-             page={page}
-             itemsCountPerPage={10}
-             totalItemsCount={10}
-              className={classes.root} color="primary"  />
+              style={{
+                backgroundColor: 'rgba(165, 165, 165, 0.886)',
+                height: "45px",
+                display: "flex",
+                alignItems: "center",
+                borderRadius: '5px'
+              }}
+              onChange={fetchTrending}
+              page={page}
+              itemsCountPerPage={10}
+              totalItemsCount={10}
+              className={classes.root} color="primary" />
           </div>
         </Fragment>
       }
